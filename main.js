e1f44d7817e8b01ea1d5eb80372229c216b81c11
SkogisAB <filip.skoglund07@outlook.com>
2024-11-26 14:53:14 +0100

    lade till lite javascript och icke hårdkodade namn etc, så att flera produkter kan läggas till via en array.

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
 let shop = document.getElementById("shop");
 
+let itemsData = [
+    {
+        id: "produkt1",
+        name: "Chocklad Chilli",
+        price: 39.90,
+        img: "img/produkt/p-chocklad-chili.webp",
+    },
+    {
+        id: "produkt2",
+        name: "Blbärspaj",
+        price: 39.90,
+        img: "img/produkt/p-blueberrypie.webp",
+    },
+    {
+        id: "produkt3",
+        name: "Disel",
+        price: 69.90,
+        img: "img/produkt/p-disel.webp",
+    },
+    {
+        id: "produkt4",
+        name: "Guacamole",
+        price: 39.90,
+        img: "img/produkt/p-guacamole.webp",
+    },
+    {
+        id: "produkt5",
+        name: "Guld",
+        price: 59.90,
+        img: "img/produkt/p-gold.webp",
+    }
+];
+
 let createShop = () => {
-    return shop.innerHTML = `
+    // Map functions tar varje item i arrayen ovan försig och upprepar det för x antal gånger (en gång för varje produkt)
+    return (shop.innerHTML = itemsData.map((x) => {
+        return `
         <div class="item">
-            <img src="img/produkt/p-chocklad-chili.webp" alt="Produkt: Tuggumi med smak av Chocklad och Chilli">
+            <img src="${x.img}" alt="Produkt: Tuggumi med smak av ${x.namn}">
             <div class="details">
-                <h3>Chocklad Chilli</h3>
+                <h3>${x.name}</h3>
                 <div class="price">
-                    <h2>39.90:-</h2>
+                    <h2>${x.price}:-</h2>
                     <i class="bi bi-basket"></i>
                 </div>
             </div>
         </div>
-    `;
-}
+    `
+    }).join(""));
+};
 
 createShop();
