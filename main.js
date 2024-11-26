f042612e48763cc6a774c34519bfd3c84af93276
SkogisAB <filip.skoglund07@outlook.com>
2024-11-26 14:22:34 +0100

    lade till en funktion som lägger in produktkort i hemsidan (med nu hårdkodade värden, dvs pris, namn osv)

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-return;
+let shop = document.getElementById("shop");
+
+let createShop = () => {
+    return shop.innerHTML = `
+        <div class="item">
+            <img src="img/produkt/p-chocklad-chili.webp" alt="Produkt: Tuggumi med smak av Chocklad och Chilli">
+            <div class="details">
+                <h3>Chocklad Chilli</h3>
+                <div class="price">
+                    <h2>39.90:-</h2>
+                    <i class="bi bi-basket"></i>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+createShop();
